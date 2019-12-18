c81848d4ec065e244570d38711893ca2a39f65b5
Shubham <shub1401@gitlab.quadeyesecurities.com>
2019-12-18 07:09:37 +0530

    First Graph with indexing explained

---
+++ data_visualization.py
@@ -0,0 +1,15 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+style.use('ggplot')
+
+start = dt.datetime(2015, 1, 1)
+end = dt.datetime.now()
+df = web.DataReader("YESBANK.BO", 'yahoo', start, end)
+df.reset_index(inplace=True)
+df.set_index("Date", inplace=True)
+print(df)
+df[['Close', 'High', 'Open', 'Low']].plot()
+plt.show()
